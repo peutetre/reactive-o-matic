5c38ee7f6735515d9d8ce203ebac2d11606f9afa
ybr <ybr@zenexity.com>
2013-01-25 11:09:49 +0100

    Application initialization

--- reactive-app/public/js/app.js
+++ reactive-app/public/js/app.js
@@ -1,1 +1,9 @@
-alert("reactivomatic")
+(function(win) {
+    var app = {};
+
+    app.init = function() {
+        console.log("init application");
+    };
+
+    win.document.addEventListener("DOMContentLoaded", app.init, false);
+})(window);
