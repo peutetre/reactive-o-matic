d522957efdf1b804e8ccf322f366abfdb1535c9b
Mr Speaker <mrspeaker@gmail.com>
2013-03-01 12:28:40 +0100

    add retrying. fix geo checking

--- reactive-app/public/js/app.js
+++ reactive-app/public/js/app.js
@@ -6,12 +6,18 @@
     var app = {
         stopped: false,
         lastPingDate: null,
-        lastLocation: {
+        location: {
             latitude: 48.87,
-            longitude: 2.33
+            longitude: 2.33,
+            interval: 5000,
+            time: -1
         },
         delay: -1,
-        uuid: new Date().getTime()
+        uuid: new Date().getTime(),
+        retry: {
+          interval: 1000,
+          count: 0
+        }
     };
 
     app.init = function() {
@@ -46,45 +52,55 @@
     }
 
     app.initWS = function() {
+        // Clear reset timer
+        clearTimeout(app.retry.timer);
+
         app.connection = new WebSocket("ws://" + window.location.host + "/hi?uuid=" + app.uuid);
+
         app.connection.onopen = function (event) {
-          app.connectionOpened();
+          app.sendPing();
         };
         app.connection.onmessage = function (event) {
-          console.log("Received event from WS : " + event.data);
           app.pong(event.data);
         }
         app.connection.onerror = function (event) {
           console.log("Received error from WS : " + event.data);
         }
-    }
 
-    app.connectionOpened = function() {
-        app.sendPing();
+        app.connection.onclose = function (event) {
+          app.logs("Connection closed.... retrying") ;
+          app.connection = null;
+          app.retry.timer = setTimeout(function() {
+            app.initWS();
+          }, app.retry.interval);
+          
+        }
     }
 
     app.sendPing = function() {
-        console.log("Sending ping")
-        ROM.geolocation.getPosition(function(latitude, longitude) {
-            app.lastLocation = {
-                latitude: latitude,
-                longitude: longitude
-            }
-        });
-        console.log("Current location : ", app.lastLocation);
+        if(Date.now() - app.location.time > app.location.interval) {
+          app.logs("Fetching geo");
+          app.location.time = Date.now();
+          ROM.geolocation.getPosition(function(latitude, longitude) {
+              app.location.latitude = latitude;
+              app.location.longitude = longitude;
+              app.location.time = Date.now();
+          });
+        }
+
         app.lastPingDate = new Date();
         var ping = { 
-            uuid: app.uuid.toString(),
             position: app.lastLocation.latitude + "," + app.lastLocation.longitude,
+            uuid: app.uuid.toString(),
+            position: app.location.latitude + "," + app.location.longitude,
             latency: app.delay
         };
         app.logs("Ping " + JSON.stringify(ping) + " ...");
         app.connection.send(JSON.stringify(ping));
-        console.log("Ping sent")
     }
 
     app.logs = function(message) {
-        console.log(message)    
+        //console.log(message)    
         var $logsContainer = $(".logs-container");
         $logsContainer.innerHTML = "<p>[" + (new Date()).toISOString() + "] " + message + "</p>" + $logsContainer.innerHTML;
     }
